aafc8d356a704d2b49f323cfde9df90cebeda199
JuanDelPueblo <edyancruz@outlook.com>
2024-06-30 16:01:00 -0700

    Add moving logic

--- src/controller.py
+++ src/controller.py
@@ -6,6 +6,13 @@ from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import ObjectsStamped, CameraInfo, Imu, Image, PoseStamped, Odometry, Path
 #!/usr/bin/env python
 
+DEPTH_SPEED = 1
+DEPTH_MOTORS_ID = [1, 2, 3, 4]
+ROTATION_SPEED = 1
+LINEAR_ROTATION_MOTORS_ID = [5, 6, 7, 8]
+LINEAR_SPEED = 1
+DELTA = 0.01
+
 def read_yaml_file(file_path):
     try:
         with open(file_path, 'r') as file:
@@ -13,8 +20,6 @@ def read_yaml_file(file_path):
     except Exception as e:
         rospy.logerr("Failed to read YAML file: %s", str(e))
         return None
-    
-SPEED = 1
 
 # TODO: Finish movement logic
 class SubController:
@@ -25,12 +30,23 @@ class SubController:
 
         self.service = rospy.Service('navigate_to_waypoint', SetBool, self.handle_navigate_request)
         self.target_pose = None
+        self.current_pose = None
         self.thrusters_pubishers = []
+        self.thruster_values = [0] * 8
+        self.moving = [False, False, False] # [depth, rotation, linear]
 
         for i in range(1,9):
             self.thrusters_publishers.append(rospy.Publisher('/thrusters/' + str(i) + '/', Vector3, queue_size=10))
 
         self.rate = rospy.Rate(10)  # 10 Hz
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+            if self.target_pose is not None:
+                # TODO: verify how often the callback is run, may have to change the rate
+                self.move_submarine(self.current_pose, self.target_pose)
+            else:
+                rospy.loginfo("Target pose is not set. Waiting for a target pose.")
+
         
     # Get topics from a YAML file then initialize subscribers
     def initialize_subscribers(self, topics_file):
@@ -47,28 +63,53 @@ class SubController:
         
     # Callback functions for the subscribers
     def zed_pose_callback(self, msg):
-        # TODO: Implement the logic to handle the pose data
         self.current_pose = msg.pose
         
     def zed_odom_callback(self, msg):
-        current_pose = msg.pose
-        if self.target_pose is not None:
-            # TODO: verify how often the callback is run, may have to change the rate
-            self.move_submarine(current_pose, self.target_pose)
-        else:
-            rospy.loginfo("Target pose is not set. Waiting for a target pose.")
+        self.current_pose = msg.pose
+        
 
     # Move the submarine to the target pose
     def move_submarine(self, current_pose, target_pose):
-        # TODO: Control depth and orientation separately
-        thruster_values = self.calculate_thruster_values(current_pose, target_pose)
-        for i in range(0, len(thruster_values)):
-            self.thrusters_publishers[i].publish(thruster_values[i])
+        self.calculate_thruster_values(current_pose, target_pose)
+        for i in range(0, len(self.thruster_values)):
+            self.thrusters_publishers[i].publish(self.thruster_values[i])
         self.rate.sleep()
 
     def calculate_thruster_values(self, current_pose, target_pose):
-        # TODO: Implement the logic to calculate the thruster values if needed
-        return [SPEED] * 8
+        if self.moving[0]:# Go up or down
+            if (current_pose.position.z - target_pose.position.z) > DELTA:
+                for i in range(0, len(DEPTH_MOTORS_ID)):
+                    self.thruster_values[DEPTH_MOTORS_ID[i]] = DEPTH_SPEED
+            elif (current_pose.position.z - target_pose.position.z) < DELTA:
+                for i in range(0, len(DEPTH_MOTORS_ID)):
+                    self.thruster_values[DEPTH_MOTORS_ID[i]] = -DEPTH_SPEED
+            else:
+                self.moving[0] = False
+        elif self.moving[1]: # Rotate
+            if (current_pose.orientation.z - target_pose.orientation.z) > DELTA:
+                for i in range(0, len(LINEAR_ROTATION_MOTORS_ID) // 2):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = ROTATION_SPEED
+                for i in range(len(LINEAR_ROTATION_MOTORS_ID) // 2, len(LINEAR_ROTATION_MOTORS_ID)):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = -ROTATION_SPEED
+            elif (current_pose.orientation.z - target_pose.orientation.z) < DELTA:
+                for i in range(0, len(LINEAR_ROTATION_MOTORS_ID) // 2):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = -ROTATION_SPEED
+                for i in range(len(LINEAR_ROTATION_MOTORS_ID) // 2, len(LINEAR_ROTATION_MOTORS_ID)):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = ROTATION_SPEED
+            else: 
+                self.moving[1] = False
+        elif self.moving[2]: # Move forward or backward
+            if (current_pose.position.x - target_pose.position.x) > DELTA:
+                for i in range(0, len(LINEAR_ROTATION_MOTORS_ID)):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = LINEAR_SPEED
+            elif (current_pose.position.x - target_pose.position.x) < DELTA:
+                for i in range(0, len(LINEAR_ROTATION_MOTORS_ID)):
+                    self.thruster_values[LINEAR_ROTATION_MOTORS_ID[i]] = -LINEAR_SPEED
+            else: 
+                self.moving[2] = False
+        else:
+            self.moving = [True, False, False]
 
     # Handle the navigate request from the service
     def handle_navigate_request(self, req):
